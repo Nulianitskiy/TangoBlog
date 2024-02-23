b50aa7abbc3803b8cc5898325c8aeb7d0a7699be
Nulianitskiy <nulianitskiy@gmail.com>
2024-02-23 09:35:10 +0300

    Начало аутентификации

--- src/main/java/com/example/tangoblog/controllers/LoginController.java
+++ src/main/java/com/example/tangoblog/controllers/LoginController.java
@@ -1,2 +1,12 @@
-package com.example.tangoblog.controllers;public class LoginController {
+package com.example.tangoblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "login";
+    }
 }
